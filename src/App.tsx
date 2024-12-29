f9ea7dfa123e0803797a27a31338103772c97c48
LadyDev2022 <hollybish10@hotmail.co.uk>
2024-12-29 18:54:41 +0000

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import { Button } from '@chakra-ui/react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div style={{ height: '100vh', padding: '1vw' }}>
-      <Button
-        colorScheme='blue'
-        size={'lg'}
-        style={{ padding: '2vw', color: 'blue' }}
-      >
-        Got to 5 - Creating a Responsive Layout (00:29)
-      </Button>
-    </div>
+    // <div style={{ height: '100vh', padding: '1vw' }}>
+    //   <Button
+    //     colorScheme='blue'
+    //     size={'lg'}
+    //     style={{ padding: '2vw', color: 'blue' }}
+    //   >
+    //     Got to 5 - Creating a Responsive Layout (00:29)
+    //   </Button>
+    // </div>
+    <Grid
+      templateAreas={{
+        /** Act as breakpoints. For more info look at: https://www.chakra-ui.com/docs/styling/responsive-design */
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // devices with a screen =< 1024px
+      }}
+    >
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Show above='lg'>
+        {/* This shall only show when the screen is above lg */}
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
