dbe441df9f2f5d687b061c3441bcb08ff63f499c
LadyDev2022 <hollybish10@hotmail.co.uk>
2024-12-29 19:23:28 +0000

    Built a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    // <div style={{ height: '100vh', padding: '1vw' }}>
-    //   <Button
-    //     colorScheme='blue'
-    //     size={'lg'}
-    //     style={{ padding: '2vw', color: 'blue' }}
-    //   >
-    //     Got to 5 - Creating a Responsive Layout (00:29)
-    //   </Button>
-    // </div>
     <Grid
       templateAreas={{
         /** Act as breakpoints. For more info look at: https://www.chakra-ui.com/docs/styling/responsive-design */
@@ -18,8 +10,8 @@ function App() {
         lg: `"nav nav" "aside main"`, // devices with a screen =< 1024px
       }}
     >
-      <GridItem area='nav' bg='coral'>
-        Nav
+      <GridItem area='nav'>
+        <NavBar />
       </GridItem>
       <Show above='lg'>
         {/* This shall only show when the screen is above lg */}
