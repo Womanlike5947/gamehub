e832e3c985d38571c37b6dd5ed3619fba0d9832e
Womanlike5947 <148662242+Womanlike5947@users.noreply.github.com>
2025-03-17 14:37:46 +0000

    15 - Loading Skeleton

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## [1.0.0] - 17-03-2024
+
+### Progress
+
+- **Started:** 14 - Getting Optimized Images
+- **Finished:** 15 - Improving User Experience with Loading Skeletons
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,11 +10,11 @@ interface IGameCard {
 
 const GameCard = ({ game }: IGameCard) => {
   return (
-    <Card borderRadius={10} overflow={'hidden'}>
+    <Card width={300} borderRadius={10} overflow={'hidden'}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
