ee840b6e5be62761575eaf1ae623fc02e711fd01
LadyDev2022 <hollybish10@hotmail.co.uk>
2025-03-24 09:55:27 +0000

    revert the vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import App from './App';
 import './index.css';
 import theme from './theme';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import { Analytics } from '@vercel/analytics/next';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
-      <Analytics />
     </ChakraProvider>
   </React.StrictMode>,
 );
